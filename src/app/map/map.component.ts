480a1ba43fce80e7f5e8b3b4e1cc2f7869439311
Bet Mendoza <sbm2391@hotmail.com>
2018-03-28 15:02:45 -0600

    radius done

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -27,33 +27,21 @@ export class MapComponent implements OnInit {
     console.log(this.radius);
   }
   mapDragEnd($event) {
-    this.dragLat=$event.coords.lat;
-    this.dragLng=$event.coords.lng;
-    console.log(this.dragLat);
-    console.log(this.dragLng);
-    console.log('dragend');
+    this.lat = $event.coords.lat;
+    this.lng = $event.coords.lng;
   }
-   // mapRadiusChange($event) {
-  //   console.log('radius change');
-  // }
+
   getDistance(lat2, lng2) {
 
     const rad = function(x) {return x * Math.PI / 180; };
     const R = 6378.137; // Radio de la tierra en km
-    const dLat = rad( lat2 - this.dragLat );
-    const dLong = rad(lng2 - this.dragLng );
-    const a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(rad(this.dragLat)) * Math.cos(rad(lat2)) * Math.sin(dLong/2) * Math.sin(dLong/2);
+    const dLat = rad( lat2 - this.lat );
+    const dLong = rad(lng2 - this.lng );
+    const a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(rad(this.lat)) * Math.cos(rad(lat2)) * Math.sin(dLong/2) * Math.sin(dLong/2);
     const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
     const d = R * c;
     this.km = d.toFixed(2); // Retorna tres decimales
     this.km= this.km*1000;
-    console.log(this.km);
     return this.km;
-  }
-  // me quede aqui
-  getMarkersRadius() {
-    this.restaurants = this.theRestaurants.filter(function(number){
-      return number % 2 === 0;
-    });
   }
 }
